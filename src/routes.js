bf35d6253ad4929a2f302926f6ee2d8a4a401a51
Geomaxv <geovannymax00@gmail.com>
2023-12-17 22:50:04 -0300

    AppSenha

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import {Home} from "./pages/home"
+import {Passwords} from "./pages/passwords"
+
+const Tab = createBottomTabNavigator();
+
+
+export function Routes() {
+    return(
+        <Tab.Navigator>
+            <Tab.Screen
+            name="home"
+            component={Home}
+            options={{headerShown: false}}
+            />
+
+            <Tab.Screen
+            name="passwords"
+            component={Passwords}
+            options={{headerShown: false}}
+            />
+        </Tab.Navigator>
+    )
+}
